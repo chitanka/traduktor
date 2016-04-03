aa2efcabf48689f8edc7a68b2d7c96d094ac13f6
Дмитрий Басавин <basavind@gmail.com>
2016-04-03 10:38:22 +0300

    refactor actionIndex: extract methods

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -1,5 +1,19 @@
 <?php
 class SiteController extends Controller {
+
+	public function actionIndex() {
+		$this->checkUser();
+
+		$this->layout = "column1";
+
+		$this->render('index', array(
+			"hot" => $this->getHot(),
+			"searchTop" => $this->getSearchTop(),
+			"announces" => $this->getAnnounces(),
+			"blog" => $this->getBlogPosts(),
+		));
+	}
+
 	private function getSearchTop() {
 		$min_size = 10;
 		$max_size = 40;
@@ -32,57 +46,6 @@ class SiteController extends Controller {
 
 		Yii::app()->cache->set($mc_key, $html, 600);
 		return $html;
-	}
-
-	public function actionIndex() {
-		if(Yii::app()->user->isGuest) {
-			if(Yii::app()->request->isPostRequest && isset($_POST["login"])) {
-				$user = new User("login");
-				$user->setAttributes($_POST["login"]);
-				$user->remember = true;
-				if($user->login()) {
-					$this->redirect("/");
-				} else {
-					Yii::app()->user->setFlash("error", $user->getError("pass"));
-				}
-			}
-			if(p()['registerType'] == "INVITE") {
-				$this->layout = "empty";
-				$this->render("index_guest");
-				return;
-			}
-		}
-
-		$this->layout = "column1";
-		$hot_key = sprintf("hot.%d.%d.%d", Yii::app()->user->ini["hot.s_lang"], Yii::app()->user->ini["hot.t_lang"], Yii::app()->user->ini["hot.img"]);
-		if(!($hot = Yii::app()->cache->get($hot_key))) {
-			$C = new CDbCriteria(array(
-				"condition" => "t.ac_read = 'a'",
-				"order" => "t.last_tr DESC NULLS LAST",
-			));
-			$C->limit = Yii::app()->user->ini["hot.img"] ? 12 : 36;
-			if(Yii::app()->user->ini["hot.s_lang"]) $C->addCondition("t.s_lang = " . Yii::app()->user->ini["hot.s_lang"]);
-			if(Yii::app()->user->ini["hot.t_lang"]) $C->addCondition("t.t_lang = " . Yii::app()->user->ini["hot.t_lang"]);
-
-			$hot = Book::model()->findAll($C);
-			Yii::app()->cache->set($hot_key, $hot, 60);
-		}
-
-		if(!($announces = Yii::app()->cache->get("announces"))) {
-			$announces = Announce::model()->with("book.cat", "book.owner", "seen")->findAll(array(
-				"condition" => "t.topics BETWEEN 80 AND 89 AND book.ac_read = 'a'",
-				"order" => "t.cdate desc",
-				"limit" => 5,
-			));
-			Yii::app()->cache->set("announces", $announces, 90);
-		}
-
-		if(!($blog = Yii::app()->cache->get("blog"))) {
-			$blog = BlogPost::model()->common()->findAll(["limit" => 10]);
-			Yii::app()->cache->set("blog", $blog, 105);
-		}
-
-  		$this->render('index', array("hot" => $hot, "searchTop" => $this->getSearchTop(), "announces" => $announces, "blog" => $blog));
 	}
 
 	public function actionPoll() {
@@ -209,5 +172,83 @@ class SiteController extends Controller {
 	public function actionClosed() {
 	    $this->layout = "offline";
 	    $this->render("closed");
+	}
+
+	/**
+	 * @return static[]
+	 */
+	private function getHot()
+	{
+		$hot_key = sprintf("hot.%d.%d.%d", Yii::app()->user->ini["hot.s_lang"], Yii::app()->user->ini["hot.t_lang"], Yii::app()->user->ini["hot.img"]);
+		if (!($hot = Yii::app()->cache->get($hot_key))) {
+			$C = new CDbCriteria(array(
+				"condition" => "t.ac_read = 'a'",
+				"order" => "t.last_tr DESC NULLS LAST",
+			));
+			$C->limit = Yii::app()->user->ini["hot.img"] ? 12 : 36;
+			if (Yii::app()->user->ini["hot.s_lang"]) $C->addCondition("t.s_lang = " . Yii::app()->user->ini["hot.s_lang"]);
+			if (Yii::app()->user->ini["hot.t_lang"]) $C->addCondition("t.t_lang = " . Yii::app()->user->ini["hot.t_lang"]);
+
+			$hot = Book::model()->findAll($C);
+			Yii::app()->cache->set($hot_key, $hot, 60);
+			return $hot;
+		}
+		return $hot;
+	}
+
+	/**
+	 * @return static[]
+	 */
+	private function getAnnounces()
+	{
+		if (!($announces = Yii::app()->cache->get("announces"))) {
+			$announces = Announce::model()->with("book.cat", "book.owner", "seen")->findAll(array(
+				"condition" => "t.topics BETWEEN 80 AND 89 AND book.ac_read = 'a'",
+				"order" => "t.cdate desc",
+				"limit" => 5,
+			));
+			Yii::app()->cache->set("announces", $announces, 90);
+			return $announces;
+		}
+		return $announces;
+	}
+
+	private function loginAttempt()
+	{
+		if (Yii::app()->request->isPostRequest && isset($_POST["login"])) {
+			$user = new User("login");
+			$user->setAttributes($_POST["login"]);
+			$user->remember = true;
+			if ($user->login()) {
+				$this->redirect("/");
+			} else {
+				Yii::app()->user->setFlash("error", $user->getError("pass"));
+			}
+		}
+	}
+
+	private function checkUser()
+	{
+		if (Yii::app()->user->isGuest) {
+			$this->loginAttempt();
+			if (p()['registerType'] == "INVITE") {
+				$this->layout = "empty";
+				$this->render("index_guest");
+				exit();
+			}
+		}
+	}
+
+	/**
+	 * @return static[]
+	 */
+	private function getBlogPosts()
+	{
+		if (!($blog = Yii::app()->cache->get("blog"))) {
+			$blog = BlogPost::model()->common()->findAll(["limit" => 10]);
+			Yii::app()->cache->set("blog", $blog, 105);
+			return $blog;
+		}
+		return $blog;
 	}
 }
