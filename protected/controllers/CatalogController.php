878710070906e9ce014db26cd5ecc37b1d9dbf28
Дмитрий Басавин <basavind@gmail.com>
2016-04-04 11:56:13 +0300

    refactor CatalogController

--- protected/controllers/CatalogController.php
+++ protected/controllers/CatalogController.php
@@ -4,16 +4,52 @@ class CatalogController extends Controller
 {
     public function actionIndex($cat_id = 0)
     {
+        list($cat, $branch) = $this->extractBranch($cat_id);
+        $tree = $this->getCategoryTree($branch);
+        $books_dp = $this->getBooksByCategory($cat);
+
+        if (in_array("ajax", $_GET)) $this->renderPartial("catalog_ajax", array("tree" => $tree));
+        else {
+            $this->render("catalog", array("cat" => $cat, "tree" => $tree, "books_dp" => $books_dp));
+        }
+    }
+
+    /**
+     * @param $cat_id
+     *
+     * @return array
+     */
+    private function extractBranch($cat_id)
+    {
         $cat_id = (int)$cat_id;
         if ($cat_id) {
             $cat = Category::model()->findByPk((int)$cat_id);
             $branch = $cat->mp;
+            return array($cat, $branch);
         } else {
             $cat = $branch = null;
+            return array($cat, $branch);
         }
+    }
 
+    /**
+     * @param $branch
+     *
+     * @return static[]
+     */
+    private function getCategoryTree($branch)
+    {
         $tree = Category::model()->tree($branch)->with("booksCount")->findAll();
+        return $tree;
+    }
 
+    /**
+     * @param $cat
+     *
+     * @return \CActiveDataProvider|null
+     */
+    private function getBooksByCategory($cat)
+    {
         if ($cat) {
             $n = count($cat->mp);
             $books_dp = new CActiveDataProvider(Book::model()->with("cat"), array(
@@ -23,13 +59,10 @@ class CatalogController extends Controller
                 ),
                 "pagination" => array("pageSize" => 50)
             ));
+            return $books_dp;
         } else {
             $books_dp = null;
-        }
-
-        if (in_array("ajax", $_GET)) $this->renderPartial("catalog_ajax", array("tree" => $tree));
-        else {
-            $this->render("catalog", array("cat" => $cat, "tree" => $tree, "books_dp" => $books_dp));
+            return $books_dp;
         }
     }
 
