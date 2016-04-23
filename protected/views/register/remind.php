d8b177b1a14065291593b0d4a75ad2587bbde98b
Дмитрий Басавин <basavind@gmail.com>
2016-04-23 17:28:42 +0300

    fix (Remind) email field

--- protected/views/register/remind.php
+++ protected/views/register/remind.php
@@ -1,19 +1,14 @@
-<?php
-	$this->pageTitle = "Напомнить пароль";
-?>
+<?php $this->pageTitle = "Напомнить пароль"; ?>
 
 <h1>Склероз</h1>
 
 <form method="post" action="/register/remind">
-	<label for="clue">
-		Введите Ваш логин или e-mail, который указывали при регистрации:
-	</label>
-	<?php
-		if (isset($_POST["clue"])):
-	?>
-	<input type="text" name="clue" id="clue" class="span6" autofocus value="<?=CHtml::encode($_POST["clue"]); ?>">
-	<?php endif; ?>
-	<p>
-	<button type="submit" class="btn btn-primary"><i class='icon-ok icon-white'></i> Восстановить</button>
-	</p>
+    <label for="clue">
+        Введите Ваш логин или e-mail, который указывали при регистрации:
+    </label>
+    <input type="text" name="clue" id="clue" class="span6" autofocus
+           value="<?php !isset($_POST["clue"]) ?: CHtml::encode($_POST["clue"]); ?>">
+    <p>
+        <button type="submit" class="btn btn-primary"><i class='icon-ok icon-white'></i> Восстановить</button>
+    </p>
 </form>
