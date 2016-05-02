b37a501c40ef91ca116daaecaf65fb522a31d33b
Дмитрий Басавин <basavind@gmail.com>
2016-05-02 21:17:54 +0300

    feat (emails) reformat remind email

--- protected/views/email/remind.php
+++ protected/views/email/remind.php
@@ -1,25 +1,18 @@
-<?php
-/**
- * @var User $user
- * @var RemindToken $token
- */
-?>
-<body>
-<p>Добрый день!</p>
-<p>
-	Забыли пароль на <?=Yii::app()->name; ?>? Вам сюда:
+<p style="text-align: center;">
+	<img style="width: 50%;"
+		 src="http://<?=Yii::app()->params["domain"]; ?>/i/logo.png" alt="Курсомир.Переводы">
 </p>
-<p>
-	<a href="<?=$token->url; ?>" style="padding:10px; background: #a1ff80; color: #005580; border-radius: 9px; "><?=$token->url; ?></a>
-</p>
-<p>
-	Если вы ничего не забывали, просто проигнорируйте это сообщение, с вашим аккаунтом ничего не случится.
-</p>
-<address style="margin-top:20px; border-top:1px solid gray; width:200px;">
-	С уважением,<br /.>
-	<a href='http://<?=Yii::app()->params["domain"]; ?>/'><?=Yii::app()->name; ?></a>
-</address>
 
-<p style="color:#777; font-style:italic;">
-	P. S. Это письмо написано искусственным интеллектом, отвечать на него не надо.
+<p>
+	Потеряли ключи от "КУРСОМИРА"? Они здесь: <br>
+	<a href="<?=$token->url; ?>" style="padding:10px; background: #a1ff80; color: #005580; border-radius: 9px; "><?=$token->url; ?></a><br>
+	И больше не теряйте.
 </p>
+<p>
+	Если всё на месте, проигнорируйте это сообщение, — с вашим аккаунтом ничего не случится.
+</p>
+<p>
+	Заходите почаще,<br>
+	"КУРСОМИР"
+</p>
+<p style='color:#777; font-style:italic;'>P. S. Это письмо написано искусственным интеллектом, отвечать на него не надо.</p>
