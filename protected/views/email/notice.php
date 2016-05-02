708e9d6d5b4af761390b1860d7a0b4e8e6d20f95
Дмитрий Басавин <basavind@gmail.com>
2016-05-02 21:25:44 +0300

    fix (emails) reformat notice email

--- protected/views/email/notice.php
+++ protected/views/email/notice.php
@@ -1,21 +1,25 @@
 <style type='text/css'>
-	p.note {color:#777; font-style:italic;}
-	address {margin-top:20px; border-top:1px solid gray; width:200px;}
-	blockquote {border-left:2px solid #777; padding:10px 0px 10px 10px;}
+    p.note { color: #777; font-style: italic; }
+    address { margin-top: 20px; border-top: 1px solid gray; width: 200px; }
+    blockquote { border-left: 2px solid #777; padding: 10px 0px 10px 10px; }
 </style>
-<base href="http://<?=Yii::app()->params["domain"]; ?>" />
+
 <body>
-<p>Добрый день, <?=$user->login; ?>!</p>
 <p>
-	<?=nl2br($Notice->render()); ?>
+    Добрый день, <?= $user->login; ?>!
+</p>
+<p>
+    <?= nl2br($Notice->render()); ?>
 </p>
 <address>
-	С уважением,<br /.>
-	<a href='http://<?=Yii::app()->params["domain"]; ?>/'><?=Yii::app()->name; ?></a>
+    С уважением,<br>
+    "КУРСОМИР"
 </address>
-
-<p class='note'>
-	P. S. Это письмо написано искусственным интеллектом, отвечать на него не надо.
-	Вы получаете эти письма потому, что включили пересылку <a href='/my/notices'>оповещений</a> на электронную почту. Отключить её можно на странице
-	<a href='/register/settings'>настроек сайта</a>.
+<p style='color:#777; font-style:italic;'>
+    P. S. Это письмо написано искусственным интеллектом, отвечать на него не надо. <br>
+    Вы получаете эти письма потому, что включили пересылку
+    <a href='http://<?= Yii::app()->params["domain"]; ?>/my/notices'>оповещений</a>
+    на электронную почту. Отключить её можно на странице
+    <a href='http://<?= Yii::app()->params["domain"]; ?>/register/settings'>настроек сайта</a>.
 </p>
+</body>
