e4fc738c3bcd02a6cacae438ba1ef9da8d8620c9
Дмитрий Басавин <basavind@gmail.com>
2016-05-02 21:39:19 +0300

    fix (emails) add styles in remind

--- protected/views/email/remind.php
+++ protected/views/email/remind.php
@@ -1,28 +1,28 @@
 <style type='text/css'>
-    p.note { color: #777; font-style: italic; }
+    .note { color: #777; font-style: italic; }
+    .token { padding:10px; background: #a1ff80; color: #005580; border-radius: 9px; }
+    .logo { text-align: center; }
+    .logo > img { width:50%; }
     address { margin-top: 20px; border-top: 1px solid gray; width: 200px; }
     blockquote { border-left: 2px solid #777; padding: 10px 0px 10px 10px; }
 </style>
-
 <body>
-<p style="text-align: center;">
-    <img style="width: 50%;"
-         src="http://<?= Yii::app()->params["domain"]; ?>/i/logo.png" alt="Курсомир.Переводы">
+<p class="logo">
+    <img src="http://<?= Yii::app()->params["domain"]; ?>/i/logo.png" alt="Курсомир.Переводы">
 </p>
 <p>
     Потеряли ключи от "КУРСОМИРА"? Они здесь: <br>
-    <a href="<?= $token->url; ?>"
-       style="padding:10px; background: #a1ff80; color: #005580; border-radius: 9px; "><?= $token->url; ?></a><br>
+    <a href="<?= $token->url; ?>" class="token"><?= $token->url; ?></a><br>
     И больше не теряйте.
 </p>
 <p>
     Если всё на месте, проигнорируйте это сообщение, — с вашим аккаунтом ничего не случится.
 </p>
-<p>
+<address>
     Заходите почаще,<br>
     "КУРСОМИР"
-</p>
-<p style='color:#777; font-style:italic;'>
+</address>
+<p class="note">
     P. S. Это письмо написано искусственным интеллектом, отвечать на него ненадо.
 </p>
 </body>
