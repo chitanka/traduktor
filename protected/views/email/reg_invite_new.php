7d074441f4e71c31395be882de35d6731cde8906
Дмитрий Басавин <basavind@gmail.com>
2016-05-02 21:43:01 +0300

    fix (emails) add styles in registration via invite email

--- protected/views/email/reg_invite_new.php
+++ protected/views/email/reg_invite_new.php
@@ -1,13 +1,14 @@
 <style type='text/css'>
-    p.note { color: #777; font-style: italic; }
+    .note { color: #777; font-style: italic; }
+    .token { padding:10px; background: #a1ff80; color: #005580; border-radius: 9px; }
+    .logo { text-align: center; }
+    .logo > img { width:50%; }
     address { margin-top: 20px; border-top: 1px solid gray; width: 200px; }
     blockquote { border-left: 2px solid #777; padding: 10px 0px 10px 10px; }
 </style>
-
 <body>
-<p style="text-align: center;">
-    <img style="width: 50%;"
-         src="http://<?= Yii::app()->params["domain"]; ?>/i/logo.png" alt="Курсомир.Переводы">
+<p class="logo">
+    <img src="http://<?=Yii::app()->params["domain"]; ?>/i/logo.png" alt="Курсомир.Переводы">
 </p>
 <p>
     НЛО приглашает вас в клуб переводчиков курсов MIT
@@ -16,12 +17,10 @@
 <p>
     Это приглашение — ваш уникальный шанс стать частью команды переводчиков.<br>
     Регистрируйтесь — и за работу:<br>
-    <a href='<?= $invite->urlAccept; ?>'><?= $invite->urlAccept; ?></a><br>
+    <a class="token" href='<?= $invite->urlAccept; ?>'>
+        <?= $invite->urlAccept; ?>
+    </a><br>
     И помните: ваш маленький шаг может создать большое будущее!
-</p>
-<p>
-    Ждём в клубе!<br>
-    "КУРСОМИР"
 </p>
 
 <?php if ($invite->message != ""): ?>
@@ -33,6 +32,11 @@
     </p>
 <?php endif; ?>
 
-<p style='color:#777; font-style:italic;'>P. S. Это письмо написано искусственным интеллектом, отвечать на него не
-    надо.</p>
+<address>
+    Ждём в клубе!<br>
+    "КУРСОМИР"
+</address>
+<p class="note">
+    P. S. Это письмо написано искусственным интеллектом, отвечать на него ненадо.
+</p>
 </body>
