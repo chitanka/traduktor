9f43a383f1cd238646185ae638ed092363356e6f
Дмитрий Басавин <basavind@gmail.com>
2016-05-02 21:44:29 +0300

    fix (emails) add styles in notice email

--- protected/views/email/notice.php
+++ protected/views/email/notice.php
@@ -1,10 +1,15 @@
 <style type='text/css'>
-    p.note { color: #777; font-style: italic; }
+    .note { color: #777; font-style: italic; }
+    .token { padding:10px; background: #a1ff80; color: #005580; border-radius: 9px; }
+    .logo { text-align: center; }
+    .logo > img { width:50%; }
     address { margin-top: 20px; border-top: 1px solid gray; width: 200px; }
     blockquote { border-left: 2px solid #777; padding: 10px 0px 10px 10px; }
 </style>
-
 <body>
+<p class="logo">
+    <img src="http://<?=Yii::app()->params["domain"]; ?>/i/logo.png" alt="Курсомир.Переводы">
+</p>
 <p>
     Добрый день, <?= $user->login; ?>!
 </p>
@@ -15,7 +20,7 @@
     С уважением,<br>
     "КУРСОМИР"
 </address>
-<p style='color:#777; font-style:italic;'>
+<p class="note">
     P. S. Это письмо написано искусственным интеллектом, отвечать на него не надо. <br>
     Вы получаете эти письма потому, что включили пересылку
     <a href='http://<?= Yii::app()->params["domain"]; ?>/my/notices'>оповещений</a>
