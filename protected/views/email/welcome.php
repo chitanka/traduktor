5350c92daf357628cc65a95e96c1f645341f93f4
Дмитрий Басавин <basavind@gmail.com>
2016-05-02 21:40:59 +0300

    fix (emails) add styles in welcome email

--- protected/views/email/welcome.php
+++ protected/views/email/welcome.php
@@ -1,17 +1,27 @@
-<p style="text-align: center;">
-	<img style="width: 50%;"
-		 src="http://<?=Yii::app()->params["domain"]; ?>/i/logo.png" alt="Курсомир.Переводы">
+<style type='text/css'>
+	.note { color: #777; font-style: italic; }
+	.token { padding:10px; background: #a1ff80; color: #005580; border-radius: 9px; }
+	.logo { text-align: center; }
+	.logo > img { width:50%; }
+	address { margin-top: 20px; border-top: 1px solid gray; width: 200px; }
+	blockquote { border-left: 2px solid #777; padding: 10px 0px 10px 10px; }
+</style>
+<body>
+<p class="logo">
+	<img src="http://<?=Yii::app()->params["domain"]; ?>/i/logo.png" alt="Курсомир.Переводы">
 </p>
 <p>
 	Вы успешно прошли регистрацию на сайте <a href='http://<?=Yii::app()->params["domain"]; ?>/'><?=Yii::app()->name; ?></a>.<br>
 	Добро пожаловать!
 </p>
 <p>
-	На всякий случай, ваш логин — <b><?=$user->login; ?></b>.
+	На всякий случай, ваш логин — <strong><?=$user->login; ?></strong>.
 </p>
-<p>
+<address>
 	Ваш,<br>
 	"КУРСОМИР"
+</address>
+<p class="note">
+	P. S. Это письмо написано искусственным интеллектом, отвечать на него ненадо.
 </p>
-
-<p style='color:#777; font-style:italic;'>P. S. Это письмо написано искусственным интеллектом, отвечать на него не надо.</p>
+</body>
