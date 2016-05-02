7baf8981b92c5c21f8ea2925a5d0d3091bfe36d4
Дмитрий Басавин <basavind@gmail.com>
2016-05-02 21:27:22 +0300

    fix (emails) reformat registration via invite

--- protected/views/email/reg_invite_user.php
+++ protected/views/email/reg_invite_user.php
@@ -1,19 +1,22 @@
-<?php
-/**
- * @var RegInvite $invite
- */
-?>
+<style type='text/css'>
+	p.note { color: #777; font-style: italic; }
+	address { margin-top: 20px; border-top: 1px solid gray; width: 200px; }
+	blockquote { border-left: 2px solid #777; padding: 10px 0px 10px 10px; }
+</style>
+
+<body>
 <p style="text-align: center;">
 	<img style="width: 50%;"
-		 src="http://<?=Yii::app()->params["domain"]; ?>/i/logo.png" alt="Курсомир.Переводы">
+		 src="http://<?= Yii::app()->params["domain"]; ?>/i/logo.png" alt="Курсомир.Переводы">
 </p>
 <p>
-	НЛО приглашает вас в клуб переводчиков курсов MIT <a href='http://<?=Yii::app()->params["domain"]; ?>/'><?=Yii::app()->name; ?></a>.
+	НЛО приглашает вас в клуб переводчиков курсов MIT
+	<a href='http://<?= Yii::app()->params["domain"]; ?>/'><?= Yii::app()->name; ?></a>.
 </p>
 <p>
 	Это приглашение — ваш уникальный шанс стать частью команды переводчиков.<br>
 	Регистрируйтесь — и за работу:<br>
-	<a href='<?=$invite->urlAccept; ?>'><?=$invite->urlAccept; ?></a><br>
+	<a href='<?= $invite->urlAccept; ?>'><?= $invite->urlAccept; ?></a><br>
 	И помните: ваш маленький шаг может создать большое будущее!
 </p>
 <p>
@@ -21,13 +24,15 @@
 	"КУРСОМИР"
 </p>
 
-<?php if($invite->message != ""): ?>
+<?php if ($invite->message != ""): ?>
 	<p>
 		Кстати, вам просили передать:<br>
 	</p>
 	<p>
-		<?=nl2br(htmlspecialchars($invite->message)); ?>
+		<?= nl2br(htmlspecialchars($invite->message)); ?>
 	</p>
 <?php endif; ?>
 
-<p style='color:#777; font-style:italic;'>P. S. Это письмо написано искусственным интеллектом, отвечать на него не надо.</p>
+<p style='color:#777; font-style:italic;'>P. S. Это письмо написано искусственным интеллектом, отвечать на него не
+	надо.</p>
+</body>
