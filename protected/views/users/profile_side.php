4b89afe8503f7d642e050966ce623778ebc6fd1d
MandorBG <mandor@mail.bg>
2017-05-25 08:34:02 +0300

    translation

--- protected/views/users/profile_side.php
+++ protected/views/users/profile_side.php
@@ -4,24 +4,24 @@
 	<dt>Пол:</dt>
 	<dd><?=Yii::app()->params["sex"][$user->sex]; ?></dd>
 
-	<dt>Родной язык:</dt>
+	<dt>Роден език:</dt>
 	<dd><?=Yii::app()->langs->inf($user->lang); ?></dd>
 
-	<dt>С нами:</dt>
+	<dt>С нас:</dt>
 	<dd><?php
 		echo "с " . Yii::app()->dateFormatter->formatDateTime($user->cdate, "long", "");
 		$d1 = new DateTime();
 		$siteAge = $d1->diff(date_create($user->cdate));
 		if($siteAge->days > 0) {
-			echo " (" . Yii::t("app", "{n}&nbsp;день|{n}&nbsp;дня|{n}&nbsp;дней", $siteAge->days) . ")";
-		} else {
-			echo " (" . $user->sexy("зарегистрировался", "зарегистрировалась", "зарегистрировалось") . " сегодня)";
+			echo " (" . Yii::t("app", "{n}&nbsp;ден|{n}&nbsp;дена|{n}&nbsp;дена", $siteAge->days) . ")";
+		} else {	//!M Да се провери третия вариант ^^^
+			echo " (" . $user->sexy("регистриран", "регистрирана", "регистрирано") . " днес)";
 		}
 		if(!$user->can(User::CAN_LOGIN)) {
-			echo "<br>Не является членом клуба. <a href='" . Yii::app()->user->getUrl("invites") . "?who=" . urlencode($user->login) . "'>Пригласить</a>.";
+			echo "<br>Не е член на клуба. <a href='" . Yii::app()->user->getUrl("invites") . "?who=" . urlencode($user->login) . "'>Покана</a>.";
 		}
 		if($user->invited_by) {
-			echo " по приглашению " . $user->invitedBy->ahref;
+			echo " по покана на " . $user->invitedBy->ahref;
 		}
 		?></dd>
 
@@ -30,26 +30,26 @@
 		if($user->n_trs > 0) {
 			$A[] =
 				"<a href='" . $user->getUrl("books") . "'>" .
-				"<strong>" . Yii::t("app", "{n} версия перевода|{n} версии перевода|{n} версий перевода", $user->n_trs) . "</strong>" .
+				"<strong>" . Yii::t("app", "{n} версия на превод|{n} версии на превод|{n} версии на превод", $user->n_trs) . "</strong>" .
 				"</a>" .
-				" с&nbsp;общим&nbsp;рейтингом&nbsp;<strong>{$user->rate_tFormatted}</strong>";
+				" с&nbsp;общ&nbsp;рейтинг&nbsp;<strong>{$user->rate_tFormatted}</strong>";
 		}
 		if($user->n_comments > 0) {
-			$A[] = "<a href='" . $user->getUrl("comments") . "'><strong>" . Yii::t("app", "{n} комментарий|{n} комментария|{n} комментариев", $user->n_comments) . "</strong></a>";
+			$A[] = "<a href='" . $user->getUrl("comments") . "'><strong>" . Yii::t("app", "{n} коментар|{n} коментара|{n} коментара", $user->n_comments) . "</strong></a>";
 		}
 		if($user->n_karma > 0) {
-			$A[] = "<a href='" . $user->getUrl("karma") . "'>Карма: {$user->rate_u}</a> (оценок: {$user->n_karma})";
+			$A[] = "<a href='" . $user->getUrl("karma") . "'>Карма: {$user->rate_u}</a> (оценки: {$user->n_karma})";
 		}
 
 		if(count($A)) {
-			echo "<dt>Деятельность:</dt>";
+			echo "<dt>Дейност:</dt>";
 			echo "<dd>" . join("<br />", $A) . "</dd>";
 		}
 	?>
 
 </dl>
 <?php if($user->id == Yii::app()->user->id) { ?>
-	<div><i class="icon-pencil"></i> <a href='<?=$user->getUrl("edit"); ?>' class='act'>Редактировать</a></div>
+	<div><i class="icon-pencil"></i> <a href='<?=$user->getUrl("edit"); ?>' class='act'>Редакция</a></div>
 <?php } ?>
 
 </div>
