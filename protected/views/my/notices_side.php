f6b632dc71c4a147a5210c3f8fccdb1c05ddc357
MandorBG <mandor@mail.bg>
2017-06-09 09:09:08 +0300

    translation

--- protected/views/my/notices_side.php
+++ protected/views/my/notices_side.php
@@ -4,10 +4,10 @@
 	 */
 ?>
 <div class="tools">
-	<h5>Оповещения</h5>
+	<h5>Известия</h5>
 	<ul class="nav nav-pills">
-		<li <?=isset($_GET["new"]) && $_GET["new"] == 1 ? "" : "class='active'"; ?>><a href="/my/notices">все</a></li>
-		<li <?=isset($_GET["new"]) && $_GET["new"] == 1 ? "class='active'" : ""; ?>><a href="?new=1">только непрочитанные</a></li>
+		<li <?=isset($_GET["new"]) && $_GET["new"] == 1 ? "" : "class='active'"; ?>><a href="/my/notices">всички</a></li>
+		<li <?=isset($_GET["new"]) && $_GET["new"] == 1 ? "class='active'" : ""; ?>><a href="?new=1">само нечетените</a></li>
 	</ul>
 
 	<form method="post" action="/my/notices_rmseen">
@@ -15,6 +15,6 @@
 		<?php
 			if(is_array($new_ids)) foreach($new_ids as $id) echo "<input type='hidden' name='x[]' value='{$id}' />";
 		?>
-		<button type="submit" class="btn"><i class="icon-remove"></i> Удалить все прочитанные</button>
+		<button type="submit" class="btn"><i class="icon-remove"></i> Премахни всички прочетени</button>
 	</form>
 </div>
