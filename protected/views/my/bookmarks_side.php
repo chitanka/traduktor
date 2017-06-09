d419d91923c7456b4849a42e15a4f227bc9524de
MandorBG <mandor@mail.bg>
2017-06-09 09:12:28 +0300

    translation

--- protected/views/my/bookmarks_side.php
+++ protected/views/my/bookmarks_side.php
@@ -1,19 +1,19 @@
 <div class='tools'>
-	<h5>Закладки</h5>
+	<h5>Отметки</h5>
 
 	<p>
-		Чтобы поменять название или удалить закладку, наведите на неё курсор и нажмите на значок <i class='icon-edit'></i>.
-		Чтобы изменить порядок закладок, таскайте их мышкой.
+		За да промените названието или да изтриете отметка, преместете курсора върху нея и натиснете <i class='icon-edit'></i>.
+		За да промените реда на отметките, ги влачете с мишката.
 	</p>
 
 	<p id="bookmarks-mass-rm">
-		<button class="btn btn-danger" onclick="E.mass_rm()"><i class="icon-remove-sign icon-white"></i> Массовое удаление</button>
+		<button class="btn btn-danger" onclick="E.mass_rm()"><i class="icon-remove-sign icon-white"></i> Масово премахване</button>
 	</p>
 	<div id="bookmarks-mass-rm-on" class="hide">
-		<p>Кликните на те закладки, которые хотите удалить.</p>
+		<p>Цъкнете върху отметките, които искате да премахнете.</p>
 		<form method="post" action="/my/bookmark_rm" id="form-mass-rm">
-			<button type="submit" class="btn btn-danger" onclick="E.mass_rm_go()" disabled><i class="icon-remove-sign icon-white"></i> Удалить отмеченные!</button>
-			<button type="button" class="btn" onclick="E.mass_rm_cancel()"><i class="icon-remove"></i> Отмена</button>
+			<button type="submit" class="btn btn-danger" onclick="E.mass_rm_go()" disabled><i class="icon-remove-sign icon-white"></i> Премахни отбелязаните!</button>
+			<button type="button" class="btn" onclick="E.mass_rm_cancel()"><i class="icon-remove"></i> Отказ</button>
 		</form>
 	</div>
 </div>
