efcd3f5375c660fc0a9a584bfc2e4efc97c5d3f0
MandorBG <mandor@mail.bg>
2017-06-09 09:15:23 +0300

    translation

--- protected/views/announces/index_side.php
+++ protected/views/announces/index_side.php
@@ -17,10 +17,10 @@
 	));
 	?>
 	<div class="control-group">
-		<label class="control-label">Из раздела каталога:</label>
+		<label class="control-label">От раздела на каталога:</label>
 		<div class="controls">
 			<select name="cat">
-				<option value="0">Не важно</option>
+				<option value="0">Без значение</option>
 				<?php
 					$tree = CHtml::listData(Category::model()->indented_list()->findAll(), "id", "title");
 					$o = array();
@@ -31,20 +31,20 @@
 	</div>
 
 	<div class="control-group">
-		<label class="control-label">Язык оригинала:</label>
+		<label class="control-label">Език на оригинала:</label>
 		<div class="controls">
 			<select name="s_lang">
-				<option value="0">Не важно</option>
+				<option value="0">Без значение</option>
 				<?=Yii::app()->langs->options(Langs::FORM_INF, $filter->s_lang); ?>
 			</select>
 		</div>
 	</div>
 
 	<div class="control-group">
-		<label class="control-label">Язык перевода:</label>
+		<label class="control-label">Език на превода:</label>
 		<div class="controls">
 			<select name="t_lang">
-				<option value="0">Не важно</option>
+				<option value="0">Без значение</option>
 				<?=Yii::app()->langs->options(Langs::FORM_INF, $filter->t_lang); ?>
 			</select>
 		</div>
@@ -54,7 +54,7 @@
 		<label class="control-label">Тема:</label>
 		<div class="controls">
 			<select name="topic">
-				<option value="0">Не важно</option>
+				<option value="0">Без значение</option>
 				<?php
 					echo CHtml::listOptions($filter->topic, Yii::app()->params["blog_topics"]["announce"], $o);
 				?>
@@ -64,17 +64,17 @@
 
 	<div class="control-group">
 		<div class="controls">
-			<label class="radio"><input type="radio" value="1" name="ready" <?=$filter->ready==1 ? "checked" : ""; ?>/>Готовые на 100%</label>
-			<label class="radio"><input type="radio" value="2" name="ready" <?=$filter->ready==2 ? "checked" : ""; ?>/>Не готовые</label>
-			<label class="radio"><input type="radio" value="0" name="ready" <?=$filter->ready==0 ? "checked" : ""; ?>/>Любые</label>
+			<label class="radio"><input type="radio" value="1" name="ready" <?=$filter->ready==1 ? "checked" : ""; ?>/>Готови на 100%</label>
+			<label class="radio"><input type="radio" value="2" name="ready" <?=$filter->ready==2 ? "checked" : ""; ?>/>Не готови</label>
+			<label class="radio"><input type="radio" value="0" name="ready" <?=$filter->ready==0 ? "checked" : ""; ?>/>Всякакви</label>
 
-			<label class="checkbox"><input type="checkbox" value="1" name="gen" <?=$filter->gen ? "checked" : ""; ?>/>Доступные для скачивания</label>
-			<label class="checkbox"><input type="checkbox" value="1" name="tr" <?=$filter->tr ? "checked" : ""; ?>/>Доступные для перевода</label>
+			<label class="checkbox"><input type="checkbox" value="1" name="gen" <?=$filter->gen ? "checked" : ""; ?>/>Достъпни за сваляне</label>
+			<label class="checkbox"><input type="checkbox" value="1" name="tr" <?=$filter->tr ? "checked" : ""; ?>/>Достъпни за превод</label>
 		</div>
 	</div>
 
 	<div class="control-group">
-		<button type="submit" class="btn btn-primary"><i class="icon-search icon-white"></i> Показать</button>
+		<button type="submit" class="btn btn-primary"><i class="icon-search icon-white"></i> Покажи</button>
 	</div>
 
 	<?php $this->endWidget(); ?>
