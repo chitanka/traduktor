deae4b4526d44c1acc08fd08aed6f56b7b621b96
borislav <b.manolov@gmail.com>
2023-04-14 07:42:16 +0200

    Remove tracker of guests

--- protected/views/book/index_side.php
+++ protected/views/book/index_side.php
@@ -155,18 +155,4 @@ FB;
 
 	<ul style="margin-top:10px"><li><?=join("</li><li>", $Tools); ?></li></ul>
 
-
-<?php if(Yii::app()->user->isGuest || !Yii::app()->user->ini_get(User::INI_ADDTHIS_OFF)): ?>
-<script type="text/javascript">(function() {
-  if (window.pluso)if (typeof window.pluso.start == "function") return;
-  if (window.ifpluso==undefined) { window.ifpluso = 1;
-    var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
-    s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
-    s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://share.pluso.ru/pluso-like.js';
-    var h=d[g]('body')[0];
-    h.appendChild(s);
-  }})();</script>
-<div class="pluso" data-background="transparent" data-options="medium,round,line,horizontal,counter,theme=04" data-services="vkontakte,facebook,twitter,pinterest,livejournal"></div>
-<?php endif ?>
-
 </div>
