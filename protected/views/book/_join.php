6a3a42cf855bf344827ed05469b3c39fad02232e
prevodach <145376111+prevodach@users.noreply.github.com>
2023-09-24 22:33:32 +0300

    Update _join.php

--- protected/views/book/_join.php
+++ protected/views/book/_join.php
@@ -4,23 +4,23 @@
 </style>
 <?php
 	if(Yii::app()->user->isGuest) {
-		echo "Вам нужно зарегистрироваться или войти на сайт и подать заявку на вступление в эту группу.";
+		echo "Трябва да се регистрирате или влезете в сайта, за да подадете заявка за влизане в групата.";
 	} else {
 ?>
 	<form method="post" action="<?=$book->getUrl("members_join"); ?>" class="well form-horizontal" style="margin-top:10px;">
 		<div id="join_btn">
 			<button type="button" onclick="$('#join_btn').hide(); $('#join_msg').show(); $('#join [name=message]').focus();" class="btn btn-success" />
-			<i class="icon-plus icon-white"></i> Вступить в группу
+			<i class="icon-plus icon-white"></i> Встъпване в групата
 			</button>
 		</div>
 		<div id="join_msg">
 			<label>
-				Ваша заявка сначала будет рассмотрена <?php echo $book->ac_membership == "m" ? "модераторами" : "создателем перевода"; ?>.
-				Вы можете написать им короткое сообщение:
+				Вашата заявка ще бъде разгледа първо от <?php echo $book->ac_membership == "m" ? "модераторите" : "създателя на превода"; ?>.
+				Можете да им напишете кратко съобщение:
 			</label>
 			<input type="text" name="message" maxlength="200" class="span4" />
-			<button type="submit" class="btn" /><i class="icon-ok"></i> Отправить заявку</button>
-			<button type="button" class="btn" onclick="$('#join_btn').show(); $('#join_msg').hide(); $('#join_msg [name=message]').focus();"><i class="icon-remove"></i> Отмена</button>
+			<button type="submit" class="btn" /><i class="icon-ok"></i> Изпращане на заявка</button>
+			<button type="button" class="btn" onclick="$('#join_btn').show(); $('#join_msg').hide(); $('#join_msg [name=message]').focus();"><i class="icon-remove"></i> Отмяна</button>
 		</div>
 	</form>
 <?php
