56969cb251f573671b6de691dc671e79bb305272
prevodach <145376111+prevodach@users.noreply.github.com>
2023-09-28 23:35:16 +0300

    Update blog_side.php

--- protected/views/bookBlog/blog_side.php
+++ protected/views/bookBlog/blog_side.php
@@ -5,7 +5,7 @@
 	 */
 ?>
 <div class='tools'>
-<h5>Блог перевода</h5>
+<h5>Блог на превода</h5>
 
 <b>Рубрики:</b>
 <ul class="nav nav-list">
@@ -15,8 +15,8 @@
 		echo "<a href='" . $book->getUrl("blog") . "?topic={$k}'>{$v}</a>";
 		echo "</li>";
 	}
-	echo "<li" . ($topic == 0 ? " class='active'" : "") . "><a href='" . $book->getUrl("blog") . "'>все</a></li>";
-	if($book->can("blog_w")) echo "<li><a href='" . $book->getUrl("blog/edit" . ($topic ? "?topic={$topic}" : "")) . "'><i class='icon icon-pencil'></i> написать пост</a></li>";
+	echo "<li" . ($topic == 0 ? " class='active'" : "") . "><a href='" . $book->getUrl("blog") . "'>всичко</a></li>";
+	if($book->can("blog_w")) echo "<li><a href='" . $book->getUrl("blog/edit" . ($topic ? "?topic={$topic}" : "")) . "'><i class='icon icon-pencil'></i> писане на пост</a></li>";
 ?>
 </ul>
 </div>
