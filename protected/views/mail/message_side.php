f0f42e32164670367b17c56f299f2fd511315e40
prevodach <145376111+prevodach@users.noreply.github.com>
2023-10-01 22:22:13 +0300

    Update message_side.php

--- protected/views/mail/message_side.php
+++ protected/views/mail/message_side.php
@@ -4,19 +4,19 @@
 	 */
 ?>
 <div class="tools">
-	<h5>Письмо</h5>
+	<h5>Писмо</h5>
 	<?php
 		echo "<p>";
-		echo $message->folder == Mail::INBOX ? "От: " : "Кому: ";
+		echo $message->folder == Mail::INBOX ? "От: " : "Получател: ";
 		echo $message->buddy->ahref . ", " . Yii::app()->dateFormatter->formatDateTime($message->cdate, "medium", "short");
 		echo "</p>";
 	?>
 
-	<p><a href="/my/mail?folder=<?=$message->folder; ?>">К списку сообщений</a></p>
+	<p><a href="/my/mail?folder=<?=$message->folder; ?>">Към списъка със съобщения</a></p>
 
-	<button type="button" class="btn btn-danger btn-small" onclick="P.rm()"><i class="icon-remove-circle icon-white"></i> Удалить</button>
-	<button type="button" class="btn btn-primary btn-small" onclick="P.unseen()" title="Сообщение будет помечено, как непрочитанное"><i class="icon-eye-close icon-white"></i> Я это не читал!</button>
+	<button type="button" class="btn btn-danger btn-small" onclick="P.rm()"><i class="icon-remove-circle icon-white"></i> Изтриване</button>
+	<button type="button" class="btn btn-primary btn-small" onclick="P.unseen()" title="Съобщението ще бъде маркирано като непрочетено"><i class="icon-eye-close icon-white"></i> Не съм го чел!</button>
 	<?php if($message->folder == Mail::INBOX): ?>
-		<a href="/my/mail/write?reply=<?=$message->id; ?>" class="btn btn-success btn-small" onclick="P.re()"><i class="icon-bullhorn icon-white"></i> Ответить</a>
+		<a href="/my/mail/write?reply=<?=$message->id; ?>" class="btn btn-success btn-small" onclick="P.re()"><i class="icon-bullhorn icon-white"></i> Отговор</a>
 	<?php endif; ?>
 </div>
