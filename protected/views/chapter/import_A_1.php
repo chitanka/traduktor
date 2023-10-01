48896626856e0944c370469e159823da2c9879cf
prevodach <145376111+prevodach@users.noreply.github.com>
2023-10-01 18:58:54 +0300

    Update import_A_1.php

--- protected/views/chapter/import_A_1.php
+++ protected/views/chapter/import_A_1.php
@@ -4,7 +4,7 @@
 	 * @var Chapter $chap
 	 */
 
-	$this->pageTitle = "Импортировать текст в перевод {$chap->book->fullTitle}";
+	$this->pageTitle = "Импортиране на текст в превода {$chap->book->fullTitle}";
 
 	Yii::app()->bootstrap->registerTabs();
 ?>
@@ -20,13 +20,13 @@ var I = {
 	}
 }
 </script>
-<h1>Импортировать текст</h1>
+<h1>Импортиране на текст</h1>
 <p>
 	Перевод: <?=$chap->book->ahref; ?>, <?=$chap->ahref; ?>
 </p>
 <?php
 	if($chap->n_verses != 0) {
-		echo "<div class='alert alert-block alert-warning'><strong>Внимание!</strong> В этой главе уже есть оригинальный текст. Если вы импортируете новый оригинал, старый текст будет уничтожен вместе с переводами и комментариями!</div>";
+		echo "<div class='alert alert-block alert-warning'><strong>Внимание!</strong> В тази глава вече има оригинален текст. Ако импортирате нов материал, старият текст ще бъде унищожен заедно с коментарите!</div>";
 	}
 ?>
 <!-- form method='post' id='form-prepare-text' class="form-inline" action="<?=$chap->getUrl("import"); ?>" enctype="multipart/form-data" -->
@@ -47,26 +47,26 @@ var I = {
 
 	<div class="tabbable">
 		<ul class="nav nav-tabs">
-			<li <?=$options->src_type == 1 ? "class='active'" : ""; ?>><a href="#1" data-toggle="tab" onclick='return I.src_type(1)'>Вставить или набрать</a></li>
-			<li <?=$options->src_type == 2 ? "class='active'" : ""; ?>><a href="#2" data-toggle="tab" onclick='return I.src_type(2)'>Из файла</a></li>
+			<li <?=$options->src_type == 1 ? "class='active'" : ""; ?>><a href="#1" data-toggle="tab" onclick='return I.src_type(1)'>Слагане или писане</a></li>
+			<li <?=$options->src_type == 2 ? "class='active'" : ""; ?>><a href="#2" data-toggle="tab" onclick='return I.src_type(2)'>От файл</a></li>
 <?php if(0) : // NOT IMPLEMENTED, TODO Импорт оригинала из HTML ?>
-	<li <?=$options->src_type == 3 ? "class='active'" : ""; ?>><a href="#3" data-toggle="tab" onclick='return I.src_type(3)'>Из интернета</a></li>
+	<li <?=$options->src_type == 3 ? "class='active'" : ""; ?>><a href="#3" data-toggle="tab" onclick='return I.src_type(3)'>От интернет</a></li>
 <?php endif; ?>
 		</ul>
 		<div class="tab-content">
 			<div id="1" class="tab-pane <?=$options->src_type == 1 ? "active" : ""; ?>">
 				<div class="control-group">
 					<textarea name='TextSource[text]' class='span8' id='TextSource_text'></textarea>
-					<p class='help-block'>Пожалуйста, не более 500 килобайт. Тексты большего размера разбейте на отдельные главы.</p>
+					<p class='help-block'>Молим, не повече от 500 килобайта. Разбийте на отделни глави по-големите текстове.</p>
 				</div>
 			</div>
 			<div id="2" class="tab-pane <?=$options->src_type == 2 ? "active" : ""; ?>">
-				<?php echo $form->fileFieldRow($options, "file", array("hint" => "Пожалуйста, только файлы .TXT не тяжелее 500 килобайт. Тексты большего размера разбейте на отдельные главы.")); ?>
+				<?php echo $form->fileFieldRow($options, "file", array("hint" => "Молим, само файлове .TXT не по-големи от 500 килобайта. Разбийте на глави по-големите текстове.")); ?>
 				<?php echo $form->dropDownListRow($options, "encoding", Yii::app()->params["encodings"]); ?>
 			</div>
 <?php if(0) : // NOT IMPLEMENTED, TODO Импорт оригинала из HTML ?>
 			<div id="3" class="tab-pane <?=$options->src_type == 3 ? "active" : ""; ?>">
-				<?php echo $form->textFieldRow($options, "url", array("placeholder" => "http://", "class" => "span7", "hint" => "Из страницы будет загружено не более 500 КБ. Если вы хотите перевести более длинную страницу, сохраните её себе на диск, откройте в браузере и скопируйте содержимое частями через буфер обмена.")); ?>
+				<?php echo $form->textFieldRow($options, "url", array("placeholder" => "http://", "class" => "span7", "hint" => "От страницата ще бъдат качени не повече от 500 КБ. Ако искате да преведете по-дълга страница, свалете я, отворете я в браузъра и копирайте по-къси парчета.")); ?>
 			</div>
 			<?php endif; ?>
 		</div>
