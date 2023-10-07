180a3db3b49293e3788f004f199c7523a2a7c4e7
prevodach <145376111+prevodach@users.noreply.github.com>
2023-10-07 13:15:24 +0300

    Update ImportOptionsSubs.php

--- protected/models/ImportOptionsSubs.php
+++ protected/models/ImportOptionsSubs.php
@@ -5,7 +5,7 @@ class ImportOptionsSubs extends CFormModel {
 	public function rules() {
 		return array(
 			// login and pass are required
-			array("src", "file", "message" => "Пожалуйста, выберите файл.", "maxSize" => 1024 * 1024, "minSize" => 1,
+			array("src", "file", "message" => "Молим, изберете файл.", "maxSize" => 1024 * 1024, "minSize" => 1,
 				"tooLarge" => "Файлът е прекалено голям", "tooSmall" => "Файлът е подозрително малък",
 			),
 			array("format", "required"),
